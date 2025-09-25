b6fa010e2aeda0de922ad2e681d6eea15fbd4c55
pef36 <pef36@txstate.edu>
2025-09-25 18:10:11 -0500

    Executed the welcome function in external.js from an internal script

--- javascript/external.js
+++ javascript/external.js
@@ -1,31 +1,15 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
-}
-// const x = "Hi";
-// const y = "welcome";
-// const out = x + y;
-// document.write(user_name);
+// arr = ["Mael","Eric","Randall","James"];
+// for (var i=0; i < arr.length; i++)
+// {
+//   document.writeln(arr[i],"<br>");
+// }
+function welcome()
+  {
+    let a="Please enter your name.";
+    let b= "Type you name here.";
 // A prompt box is used to prompt users to input a value before entering a page.
-user_name = window.prompt("Please enter your name", "Type your name here");
-document.write(user_name);
-
-My name is Ashlyn <br>
-<div id="copyright">
-  <script>
-    //copyright
-    var today= newDate(Sept 25th);
-    var year = today.getFullYear(2025);
-    document.writeIn("copyright &copy;" + year);
-  </script>
-</div>
-#copyright {
-  bottom: 20px;
-  margin: 0;
-  padding: 0;
-  text-align: center;
-  position: relative;
-  left: 50%;
-  transform: translateX(-50%)translateY(-50%);
+  user_name = window.prompt(a, b);
+  message = "<h1>Hello, welcome to my webpage, " + user_name + "!</h1>"
+  return message
 }
+document.write(welcome());
