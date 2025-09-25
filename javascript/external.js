999a61b637ce48240ecc0670f2ea3c712cb36eb0
pef36 <pef36@txstate.edu>
2025-09-25 13:53:31 -0500

    Saved username 2

--- javascript/external.js
+++ javascript/external.js
@@ -3,10 +3,10 @@ for (var i=0; i < arr.length; i++)
 {
   document.writeln(arr[i],"<br>");
 }
-const x = "Hi";
-const y = "welcome";
-const out = x + y;
-document.write(user_name);
+// const x = "Hi";
+// const y = "welcome";
+// const out = x + y;
+// document.write(user_name);
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
