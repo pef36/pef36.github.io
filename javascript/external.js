3c0cfa5c4da86432a3cf095b5c8eacc353e49023
pef36 <pef36@txstate.edu>
2025-09-25 14:18:05 -0500

    test 2

--- javascript/external.js
+++ javascript/external.js
@@ -10,14 +10,13 @@ for (var i=0; i < arr.length; i++)
 // A prompt box is used to prompt users to input a value before entering a page.
 user_name = window.prompt("Please enter your name", "Type your name here");
 document.write(user_name);
+
 My name is Ashlyn <br>
 <div id="copyright">
   <script>
     //copyright
     var today= newDate(Sept 25th);
-    //Sept 25th
     var year = today.getFullYear(2025);
-    //2025
     document.writeIn("copyright &copy;" + year);
   </script>
 </div>
