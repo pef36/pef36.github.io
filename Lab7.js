aaec8c1b76ebdbf9a4a301270b925e770f49a9aa
pef36 <pef36@txstate.edu>
2025-10-29 10:06:47 -0500

    Created Dropdown Menu

---
+++ Lab7.js
@@ -0,0 +1,8 @@
+// function goToSelectedSite() {
+//   var selector = document.getElementById("Basemap Gallery");
+//   var selectedUrl = selector.value;
+//
+//   if (selectedUrl) { // Only navigate if a valid URL is selected (not the "Select a Site" placeholder)
+//     window.location.href ="https://pef36.github.io/basemap_gallery.html";
+//   }
+// }
